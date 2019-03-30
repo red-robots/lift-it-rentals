a8eb76b060a8b8c715c20bc9903236eaa8236303
lisadebona <annalisa.quiamco@gmail.com>
2019-03-30 00:01:29 -0600

    Updated custom.js

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -31,12 +31,12 @@ jQuery(document).ready(function ($) {
 	          // Callback after animation
 	          // Must change focus!
 	          var $target = $(target);
-	          $target.focus();
+	          //$target.focus();
 	          if ($target.is(":focus")) { // Checking if the target was focused
 	            return false;
 	          } else {
 	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-	            $target.focus(); // Set focus again
+	            //$target.focus(); // Set focus again
 	          };
 	        });
 	      }
@@ -117,7 +117,7 @@ jQuery(document).ready(function ($) {
 		var hash_name = target.selector;
 		var pageURL = siteURL + '/about/our-team/' + hash_name;
 		$(this).attr('href',pageURL);
-		
+
 	});
 
 
